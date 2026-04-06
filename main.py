df64feb3ceb375cf77b21ef77ef0c96faac9cbf2
murodxoja080-bit <murodxoja080@gmail.com>
2026-04-06 22:18:32 +0300

    updape1

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ async def main():
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
-    asyncio.run(main)
+    asyncio.run(main())
     from handlers import music, middlewares
 # ...
 dp.message.middleware(middlewares.AntiFloodMiddleware)
