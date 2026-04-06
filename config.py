fffc62bc11bef182e64ffb8fd134302647c1d43a
murodxoja080-bit <murodxoja080@gmail.com>
2026-04-06 18:38:57 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+API_TOKEN = '8769670116:AAFzJf9REpF8l2UNYI1hLtgffUmjU1V-69I'
+ADMIN_ID = 7085980511
+BRAND = "KHUJANOV"
+
+# Premium Dizayn elementlari
+DIVIDER = "<b>————————————————————</b>"
+SIGNATURE = f"\n\n<blockquote>© 2026 <b>{BRAND}</b> | Barcha huquqlar himoyalangan.</blockquote>"
+
+# API ssilkalari (Avtomatik qidiruv uchun)
+LIBRARY_URL = "https://ziyouz.uz"
+SCHOOL_BASE_URL = "https://e-darslik.uz"
